9f636b4e17a4eb533a47f754ad6eb8b8dd7caa41
3eze3 <ezekics33@gmail.com>
2024-11-12 10:17:36 -0500

    Complete the login function

--- build/js/login.mjs
+++ build/js/login.mjs
@@ -1,2 +1,7 @@
 export function login() {
+    const btn = document.querySelector(".form__btn");
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        window.location.replace("https://www.instagram.com");
+    });
 }
--- build/js/main.mjs
+++ build/js/main.mjs
@@ -1,5 +1,7 @@
+import { login } from "./login.mjs";
 import { fadeout } from "./utils/animations/fadeout.mjs";
 import { animaitonLogin, toggleBtn } from "./utils/animations/form.mjs";
 fadeout();
 animaitonLogin();
 toggleBtn();
+login();
